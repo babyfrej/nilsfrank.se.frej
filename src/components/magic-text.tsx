f74aa66617d8fe1ed81d2209284b367eeb7c7e7f
Niklas <niklas.frank@tretton37.com>
2023-08-06 08:50:38 +0200

    fix: add types to magic-text

--- src/components/magic-text.tsx
+++ src/components/magic-text.tsx
@@ -1,3 +1,6 @@
-export default function MagicText({ children }) {
+import { PropsWithChildren } from "react";
+
+type Props = PropsWithChildren<{}>;
+export default function MagicText({ children }: Props): JSX.Element {
   return <span className="magic-text">{children}</span>;
 }
