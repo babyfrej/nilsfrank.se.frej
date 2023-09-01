df4f05419ca8e935a306198a1f83baf654468a92
Niklas <niklas.frank@tretton37.com>
2023-09-01 20:54:40 +0200

    feat: move claim up one path level

--- src/app/wishlist/[id]/route.ts
+++ src/app/wishlist/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import z from "zod";
+
 export async function GET(
   req: Request,
   { params: { id } }: { params: { id: string } },
@@ -14,4 +16,44 @@ export async function GET(
     },
   });
   return NextResponse.json({ data });
+}
+
+const codeSchema = z
+  .string({
+    required_error: "Saknar en inbjudan",
+    invalid_type_error: "Inbjudan är inte giltig",
+  })
+  .email();
+const formFields = z.object({
+  amount: z.preprocess(Number, z.number({ required_error: "Saknar belopp" })),
+});
+
+export async function POST(
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } },
+) {
+  const code = codeSchema.safeParse(
+    req.cookies.get(process.env.COOKIE_CODE)?.value,
+  );
+  if (!code.success) {
+    return NextResponse.json(code.error.errors, { status: 400 });
+  }
+  const data = formFields.parse(await req.json());
+  await prisma.wishlistClaim.upsert({
+    where: {
+      email_wishlistId: {
+        email: code.data,
+        wishlistId: id,
+      },
+    },
+    create: {
+      wishlistId: id,
+      email: code.data,
+      amount: data.amount,
+    },
+    update: {
+      amount: data.amount,
+    },
+  });
+  return NextResponse.json({});
 }
