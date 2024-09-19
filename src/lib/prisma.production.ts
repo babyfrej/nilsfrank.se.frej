c1cc3d7201d532c1f8da7fb306c86c45a45cfbe0
Niklas <niklas.frank@gmail.com>
2024-09-19 09:14:18 +0200

    fix: preserve prisma client

--- src/lib/prisma.production.ts
+++ src/lib/prisma.production.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client";
+mport { createClient } from "@libsql/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@prisma/client";
 
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,24 @@
-import type { PrismaClient } from "@prisma/client";
-import createDevelopment from "./prisma.development";
-import createProduction from "./prisma.production";
+import { PrismaClient } from "@prisma/client";
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+
+const globalPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
 let prisma: PrismaClient;
-
-switch (process.env.APP_ENV) {
-  case "development":
-  case "test":
-    prisma = createDevelopment();
-  case "production":
-    prisma = createProduction();
+if (process.env.APP_ENV === "production") {
+  const libsql = createClient({
+    url: `${process.env.TURSO_DATABASE_URL}`,
+    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+  });
+  const adapter = new PrismaLibSQL(libsql);
+  prisma = new PrismaClient({ adapter });
+} else {
+  if (!globalPrisma.prisma) {
+    globalPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalPrisma.prisma;
 }
 
 export default prisma;
