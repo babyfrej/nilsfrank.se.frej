09d206b70e6c76775b10f390cb72ee195dab85ad
Niklas <niklas.frank@gmail.com>
2024-09-20 22:15:57 +0200

    fix swish image generation

--- src/app/api/qr/[id]/img.png/route.ts
+++ src/app/api/qr/[id]/img.png/route.ts
@@ -1,36 +1,86 @@
 import prisma from "@/lib/prisma";
+import { ClaimType } from "@/types/claim-type";
 import type { NextRequest } from "next/server";
 
-export const revalidate = 60 * 60 * 24 * 7 * 365;
+type SwishBodyRequest = {
+  format: "jpg" | "png" | "svg";
+  payee?: { value: string; editable: boolean };
+  amount?: { value: number; editable: boolean };
+  message?: { value: string; editable: boolean };
+  size?: number;
+  border?: number;
+  transparent?: boolean;
+};
+
+const MESSAGE_ALLOWED_CHARS = /[^a-öA-Ö0-9?!() .,\-:;]/g;
+const MESSAGE_MAX_LENGTH = 50;
+
+export const revalidate = 60 * 60 * 24 * 365;
+
+export async function generateStaticParams() {
+  return prisma.wishlist.findMany({
+    where: { claimType: ClaimType.DONATE, eventId: process.env.EVENT_ID },
+    select: { id: true },
+  });
+}
+
 export async function GET(
-  req: NextRequest,
+  _req: NextRequest,
   { params: { id } }: { params: { id: string } },
 ) {
   if (!id) {
     return new Response("Not found", { status: 404 });
   }
-  const item = await prisma.wishlist.findFirst({
-    where: { id: id },
-    select: { title: true },
-  });
+  const [item, contact] = await prisma.$transaction([
+    prisma.wishlist.findFirst({
+      where: { id: id, eventId: process.env.EVENT_ID },
+      select: { title: true, price: true },
+    }),
+    prisma.eventContact.findFirst({
+      where: { event: { id: process.env.EVENT_ID } },
+      select: { phone: true },
+    }),
+  ]);
   if (!item) {
     return new Response("Not found", { status: 404 });
   }
+  if (!contact?.phone) {
+    return new Response("Contact Not Found", { status: 500 });
+  }
 
-  const qr = await fetch("https://api.swish.nu/qr/v2/prefilled", {
-    method: "post",
-    headers: {
-      Accept: "*/*",
-      "Content-Type": "application/json",
+  const body = {
+    format: "png",
+    payee: {
+      value: contact.phone,
+      editable: false,
     },
-    body: JSON.stringify({
-      payee: "46722133743",
-      amount: { value: 200, editable: true },
-      size: 400,
-      message: { value: item.title },
-    }),
-  });
-  return new Response(await qr.blob(), {
+    amount: {
+      value: item.price ?? 200,
+      editable: true,
+    },
+    message: {
+      value: item.title
+        .replaceAll(MESSAGE_ALLOWED_CHARS, "")
+        .slice(0, MESSAGE_MAX_LENGTH),
+      editable: false,
+    },
+    size: 400,
+    border: 1,
+  } satisfies SwishBodyRequest;
+
+  const qr = await fetch(
+    "https://mpc.getswish.net/qrg-swish/api/v1/prefilled",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  );
+  const res = await qr.blob();
+
+  return new Response(res, {
     headers: {
       "Content-Type": "image/png",
     },
